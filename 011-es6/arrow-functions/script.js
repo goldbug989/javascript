e110e70755d007c2c813eb3e8d44eaf01c896a99
goldbug989 <dyannuzzi@gmail.com>
2020-09-27 20:17:17 -0500

    coding arrow functions in es6 folder 011

--- 011-es6/arrow-functions/script.js
+++ 011-es6/arrow-functions/script.js
@@ -18,3 +18,49 @@ console.log(ages6);
 //combine with template literals from es6
 ages6 = years.map((el,index) => `age element ${index+1}: ${now.getFullYear()-el}.`);
 console.log(ages6);
+
+
+//es5....this.position and this.color will be undefined
+//       this points to default object..window for functions
+
+//for a work around use a variable assigned to this inside the function
+//                          see self variable
+
+//           if the self variable wasn't there and you used this keyword
+//                    in its place you would get undefined
+//
+var box5 = {
+  color:'green',
+  position: 1,
+  clickMe: function(){
+
+    var self = this;
+    document.querySelector('.green').addEventListener('click',
+    function(){
+      var str = 'this is box number ' +
+      self.position + ' and it is ' +
+      self.color;
+      alert(str);
+    });
+  }
+}
+//box5.clickMe();
+
+//es6.........arrow functions have use of lexical this keyword
+//
+const box6 = {
+  color:'green',
+  position: 1,
+  clickMe: function(){
+
+    document.querySelector('.green').addEventListener('click',
+      () => {
+          var str = 'this is box number ' +
+          this.position + ' and it is ' +
+          this.color;
+          alert(str);
+      });
+
+  }
+}
+box6.clickMe();
