20c52992676b0a99feedaf78ab2436c824a9e1d6
goldbug989 <dyannuzzi@gmail.com>
2020-10-01 13:30:45 -0500

    folder 011 es6 adds subfolder on Classes

--- 011-es6/classes/script.js
+++ 011-es6/classes/script.js
@@ -1,3 +1,46 @@
 //**********************************************
 //Classes
 //**********************************************
+//make it easier to use inherit than fn prototypes
+
+//es5
+var Person5 = function(first,last,byear){
+  this.first=first;
+  this.last=last;
+  this.byear = byear;
+}
+Person5.prototype.calcAge =
+function(){
+  var age = new Date().getFullYear() - this.byear;
+  console.log(age);
+}
+var john5 = new Person5('john','smith',1990);
+john5.calcAge();
+
+//ES6
+//*************************************************
+class Person6{
+  constructor (first,last,byear){
+
+    this.first=first;
+    this.last=last;
+    this.byear=byear;
+  }
+
+  calcAge(){
+
+    var age = new Date().getFullYear() -
+    this.byear;
+    console.log(age);
+  }
+
+  static greeting(){// can only call from Person6 .not instance
+    console.log('hey there i am not inherited by person6 instance');
+  }
+
+
+}
+
+let sally = new Person6('sally','struthers',1970);
+Person6.greeting();
+sally.calcAge();
