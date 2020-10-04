2030a283afc77c1b65a2ff7b36a495ac4a3d0cef
goldbug989 <dyannuzzi@gmail.com>
2020-10-04 17:23:48 -0500

    adds folder 012 async js

---
+++ 012async-js/promise/script.js
@@ -0,0 +1,46 @@
+//8888888888888888888888888888888888888888888888888888888888
+//              promise is an object
+//      that keeps track if an event has happened
+//      determines what happents after event
+//8888888888888888888888888888888888888888888888888888888888
+
+//     promise states---------------
+//            pending  => event happens =>
+//            settled/resolved =>
+//            fulfilled or rejected
+
+//promise params functions resolve and reject
+const getIds = new Promise((resolve, reject) => {//executor fn
+    //async code
+    setTimeout(() => {
+      //call resolve marks promise as success
+      resolve([523,883,432,974]);//returns
+      //dont need a reject method because timeout will always work
+
+    },1500);//passing in callback fn and 1500ms timeout
+});
+
+const getRecipe = recID => {
+  return new Promise((resolve,reject) => {
+     setTimeout((id) => {
+
+        const recipe = {title:'tomato pasta',
+        publisher:'john Adams'};
+        resolve(`${id}: ${recipe.title}`);
+     },1500,recID);
+  });
+};
+
+
+//pass in callback fn for when promise fulfilled
+getIds
+.then(IDs => {//always arg of what returned from fullfilled promise
+  console.log(IDs);
+  return getRecipe(IDs[2])
+})
+.then(recipe => {//no callback hell
+  console.log(recipe);
+})
+.catch(error => {//incase of reject you get this
+  console.log('error!!!');
+});
