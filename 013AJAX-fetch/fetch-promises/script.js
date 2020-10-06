cf242b4d50328b3ff8dfb36f0550b1913c2571f9
goldbug989 <dyannuzzi@gmail.com>
2020-10-06 10:41:06 -0500

    ajax w/fetch folder012

--- 013AJAX-fetch/fetch-promises/script.js
+++ 013AJAX-fetch/fetch-promises/script.js
@@ -13,16 +13,19 @@ function getWeather(woeid){
 fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`)
 .then(result => {
     console.log(result);
+    //result is a readable stream, convert to JSON
     return result.json();
 })
 .then(data =>{
   console.log(data);
   const today = data.consolidated_weather[0];
-  console.log(`    --${data.title}--
-    ${today.applicable_date}
-    ${today.weather_state_name}
+  console.log(`  ${data.title} ${today.applicable_date}
+-----------------------
+   Now: ${parseInt(today.the_temp*9/5 +32)}F ${today.weather_state_name}
     LO: ${parseInt(today.min_temp*9/5 +32)}F
-    HI: ${parseInt(today.max_temp*9/5 +32)}F `)
+    HI: ${parseInt(today.max_temp*9/5 +32)}F
+ Humid: ${today.humidity}%`)
+
 
 })
 
