4ab07f834465c6e7f6635628e0e0175b0a7fa62b
goldbug989 <dyannuzzi@gmail.com>
2020-10-07 14:52:30 -0500

    adds 013 recipe app folder and npm

---
+++ 014recipe-app/webpack.config.js
@@ -0,0 +1,10 @@
+//eport this configuration
+module.exports = {
+    entry: './src/js/index.js',
+    output:{
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: 'bundle.js'
+    },
+    mode: 'development'
+
+};
