835a062677a6dc2a844272df593d347cdef42489
goldbug989 <dyannuzzi@gmail.com>
2020-10-08 12:25:15 -0500

    adds webpack to package.json folder014 recipe

--- 014recipe-app/src/js/index.js
+++ 014recipe-app/src/js/index.js
@@ -1,1 +1,4 @@
 // Global app controller
+import x from './test';
+
+console.log(x + ' imorted from one module to another');
--- 014recipe-app/webpack.config.js
+++ 014recipe-app/webpack.config.js
@@ -1,10 +1,15 @@
 //eport this configuration
+//4 core concepts entry, output,loaders,plugins
+const path = require('path');
+
 module.exports = {
     entry: './src/js/index.js',
     output:{
+        //need absolute path, use path package 
+        //__dirname is current absolute path, join with dist/js
         path: path.resolve(__dirname, 'dist/js'),
         filename: 'bundle.js'
     },
-    mode: 'development'
+
 
 };
