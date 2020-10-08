d477d349cae7827dfb6195ed5d33a00789a1320d
goldbug989 <dyannuzzi@gmail.com>
2020-10-08 13:11:29 -0500

    014recipe app  configuring webpack

--- 014recipe-app/webpack.config.js
+++ 014recipe-app/webpack.config.js
@@ -1,15 +1,25 @@
 //eport this configuration
 //4 core concepts entry, output,loaders,plugins
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/js/index.js',
     output:{
         //need absolute path, use path package 
         //__dirname is current absolute path, join with dist/js
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
     },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            filename:'index.html',
+            template: './src/index.html'
+        })
+    ]
 
 
 };
