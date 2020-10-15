936bf411149480f88f5a143e991a1301d3118c22
goldbug989 <dyannuzzi@gmail.com>
2020-10-15 12:08:45 -0500

    working on folder 014 searchView

--- 014recipe-app/src/js/index.js
+++ 014recipe-app/src/js/index.js
@@ -1,5 +1,43 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+/**global state of the app
+ * -search object
+ * -current recipe object
+ * -shopping list object
+ * -liked recipes
+ */
+const state = {};
+
+
+const controlSearch = async() => {
+    //get query from view
+    const query = searchView.getInput(); //todo
+
+    if (query){
+        //2 new search object and add to state
+        state.search = new Search(query);
+
+        //3 prepare ui for results
+
+        //4 search for recipes
+        await state.search.getResults();
+
+        //5 render results on UI
+        console.log(state.search.result);
+ 
+
+    }
+
+
+}
+
+
+elements.searchForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    controlSearch();
+});
+
+
+
